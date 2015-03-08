cf3a56dbb3b9a9da6da4de56f65ebd52f2fa7db4
Peter Cai <xqsx43cxy@126.com>
2015-03-08 15:42:08 +0800

    BlackListActivity: Make items clickable

--- app/src/main/java/info/papdt/lolistat/ui/adapter/AppAdapter.java
+++ app/src/main/java/info/papdt/lolistat/ui/adapter/AppAdapter.java
@@ -62,14 +62,23 @@ public class AppAdapter extends BaseAdapter
 		TextView text = (TextView) v.findViewById(R.id.name);
 		text.setText(app.title);
 		
-		CheckBox check = (CheckBox) v.findViewById(R.id.check);
+		final CheckBox check = (CheckBox) v.findViewById(R.id.check);
 		check.setOnCheckedChangeListener(null);
+		v.setOnClickListener(null);
 		check.setChecked(app.checked);
 		
 		check.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 			@Override
 			public void onCheckedChanged(CompoundButton button, boolean checked) {
 				app.checked = checked;
+			}
+		});
+		
+		v.setClickable(true);
+		v.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				check.setChecked(!check.isChecked());
 			}
 		});
 		
